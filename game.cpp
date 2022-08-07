3bcd33ef934a1172fe32e7df53015d26c0d6a970
Hadi Bux <43892592+hadibuxm@users.noreply.github.com>
2022-08-08 00:00:39 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -344,7 +344,7 @@ bm = 'X';
         int choice;
         cout << "choose box " << endl;
         cout << "1. top left" << endl << "2.top middle" << endl << "3.top right" << endl;
-        cout << "4. middle right" << endl << "5. middle" << endl << "6. middle left" << endl;
+        cout << "4. middle left" << endl << "5. middle" << endl << "6. middle right" << endl;
         cout << "7. bottom left" << endl << "8.bottom middle" << endl << "9.bottom right" << endl;
         cin >> choice;
         switch (choice) {
